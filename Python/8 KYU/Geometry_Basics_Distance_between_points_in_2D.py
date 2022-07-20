6914ee83729c6d08ace71660202d1a6a1083338b
Eduardo Longen Corrêa <e.lcorrea256@gmail.com>
2022-07-20 11:21:58 -0300

    completed exercise

---
+++ Python/8 KYU/Geometry_Basics_Distance_between_points_in_2D.py
@@ -0,0 +1,12 @@
+# This series of katas will introduce you to basics of doing geometry with computers.
+# 
+# Point objects have x and y attributes (X and Y in C#) attributes.
+# 
+# Write a function calculating distance between Point a and Point b.
+# 
+# Tests round answers to 6 decimal places.
+
+from math import hypot
+
+def distance_between_points(a, b):
+    return hypot( a.x - b.x, a.y - b.y )
