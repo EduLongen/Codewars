7379c472456a8b31b540f964198b6cfddcf14ffa
Eduardo Longen Corrêa <e.lcorrea256@gmail.com>
2022-07-26 17:38:40 -0300

    completed exercise

---
+++ PHP/7 KYU/Sum_of_angles.php
@@ -0,0 +1,9 @@
+<?php
+
+/*
+Find the total sum of internal angles (in degrees) in an n-sided simple polygon. N will be greater than 2.
+*/
+
+function angle(int $n): int {
+    return ($n - 2) * 180;
+}
